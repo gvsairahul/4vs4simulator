36cfcf74385da478425efa9976811c93e9da759f
rahul <gvsairahul@gmail.com>
2020-05-09 03:31:40 -0500

    Complete stats based

---
+++ out_sim1.py
@@ -0,0 +1,100 @@
+from random import choices
+
+def toss():
+    results=['Heads','Tails']
+    weights=[0.5,0.5]
+    return choices(results,weights=weights)[0]
+
+def out_calculator(batsman,bowler):
+    runs_scored=batsman['runs_scored']
+    average=batsman['average']
+    bowlers_average=bowler['average']
+    runs_conceded=bowler['runs_conceded']
+    wickets_taken=bowler['wickets_taken']
+    results=["out","notout"]
+    if (runs_scored/average)<1.0:
+        out_batsmen=0.1*(runs_scored/average)
+    else:
+        out_batsmen=0.1
+    if (runs_conceded/(bowlers_average*(wickets_taken+1)))<1.0:
+        out_bowler=0.1*(runs_conceded/(bowlers_average*(wickets_taken+1)))
+    else:
+        out_bowler=0.1
+    distribution=[max(out_bowler,out_batsmen),1-max(out_bowler,out_batsmen)]
+    return choices(results,weights=distribution)[0]
+
+def runs_calculator(balls,batsman,bowler):
+    runs_scored=batsman['runs_scored']
+    balls_faced=batsman['balls_faced']
+    strikerate=batsman['strikerate']/100
+    runs_conceded=bowler['runs_conceded']
+    balls_bowled=bowler['balls_bowled']
+    ratio=batsman['ratio']
+    economy=bowler['economy']/6
+    results=[]
+    weights=[]
+    for i in range(7):
+        if i !=5 and i!=3:
+            results.append(i)
+            weights.append(0.0)
+    possible_cases_batsman=[]
+    possible_cases_bowler=[]
+    if balls_faced!=0:
+        present_strike_rate=runs_scored/balls_faced
+    else:
+        present_strike_rate=strikerate
+    if balls_bowled!=0:
+        present_economy=runs_conceded/balls_bowled
+    else:
+        present_economy=economy
+    if present_strike_rate<strikerate:
+        for i in results:
+            if i>strikerate:
+                possible_cases_batsman.append(i)
+    else:
+        for i in results:
+            if i<strikerate:
+                possible_cases_batsman.append(i)
+    if present_economy<economy:
+        for i in results:
+            if i>economy:
+                possible_cases_bowler.append(i)
+    else:
+        for i in results:
+            if i<economy:
+                possible_cases_bowler.append(i)
+    for i in range(len(results)):
+        if (results[i]==0):
+            if results[i] in possible_cases_batsman:
+                weights[i]+=0.5*(1/len(possible_cases_batsman))
+            if results[i] in possible_cases_bowler:
+                weights[i]+=0.5*(1/len(possible_cases_bowler))
+        elif (results[i]<=2) and (results[i]!=0):
+            if results[i] in possible_cases_batsman:
+                if balls<=36 and balls>=90:
+                    weights[i]+=0.1*(1/len(possible_cases_batsman))
+                else:
+                    weights[i]+=0.5*(1/len(possible_cases_batsman))
+            if results[i] in possible_cases_bowler:
+                if balls<=36 and balls>=90:
+                    weights[i]+=0.1*(1/len(possible_cases_batsman))
+                else:
+                    weights[i]+=0.5*(1/len(possible_cases_batsman))
+        elif results[i]==4:
+            if results[i] in possible_cases_batsman:
+                weights[3]+=0.5*(1/len(possible_cases_batsman))
+            if results[i] in possible_cases_bowler:
+                weights[3]+=0.5*(1/len(possible_cases_bowler))
+        else:
+            if results[i] in possible_cases_batsman:
+                weights[4]+=(0.5*ratio*weights[3])
+            if results[i] in possible_cases_bowler:
+                weights[4]+=(0.5*ratio*weights[3])
+    return choices(results,weights=weights)[0]
+
+def change_batsman(current_batsmen,current_batsmen_id):
+    if current_batsmen_id==min(current_batsmen):
+        current_batsmen_id=max(current_batsmen)
+    else:
+        current_batsmen_id=min(current_batsmen)
+    return current_batsmen_id
---
+++ second_innings.py
@@ -0,0 +1,124 @@
+import csv, os, time
+from out_sim1 import out_calculator, runs_calculator, change_batsman, toss
+def chase(target,attributes):
+    print("The target is "+str(target))
+    current_batsmen=[]
+    current_bowler_id=0
+    team_score=0
+    team_wickets=0
+    for j in range(len(attributes['batsmen'])):
+        if attributes['batsmen'][j]['balls_faced']==0 and (j not in current_batsmen):
+            print(str(j)+'  '+attributes['batsmen'][j]['name'])
+    choser=int(input("Choose the  batsman on strike:"))
+    current_batsmen.append(choser)
+    current_batsmen_id=choser
+    for j in range(len(attributes['batsmen'])):
+        if attributes['batsmen'][j]['balls_faced']==0 and (j not in current_batsmen):
+            print(str(j)+'  '+attributes['batsmen'][j]['name'])
+    choser=int(input("Choose the  batsman on non strike:"))
+    current_batsmen.append(choser)
+    for i in range(len(attributes['bowlers'])):
+        print(str(i)+' '+attributes['bowlers'][i]['name'])
+    for i in range(len(attributes['bowlers'])):
+        print(str(i)+' '+attributes['bowlers'][i]['name'])
+    choser=int(input("Choose the id of bowler from above:"))
+    current_bowler_id=choser
+    print('\n')
+
+    for i in range(1,121):
+        result=out_calculator(attributes['batsmen'][current_batsmen_id],attributes['bowlers'][current_bowler_id])
+        attributes['batsmen'][current_batsmen_id]['balls_faced']+=1
+        attributes['bowlers'][current_bowler_id]['balls_bowled']+=1
+        if result=='notout':
+            runs=runs_calculator(i,attributes['batsmen'][current_batsmen_id],attributes['bowlers'][current_bowler_id])
+            print("Ball "+str(i)+' - '+attributes['bowlers'][current_bowler_id]['name']+" to "+attributes['batsmen'][current_batsmen_id]['name']+' : '+str(runs))
+            team_score+=runs
+            attributes['batsmen'][current_batsmen_id]['runs_scored']+=runs
+            attributes['bowlers'][current_bowler_id]['runs_conceded']+=runs
+            if runs==0:
+                attributes['bowlers'][current_bowler_id]['dots']+=1
+            if runs==4:
+                attributes['batsmen'][current_batsmen_id]['fours']+=1
+            elif runs==6:
+                attributes['batsmen'][current_batsmen_id]['sixes']+=1
+            if runs==1:
+                current_batsmen_id=change_batsman(current_batsmen,current_batsmen_id)
+            if team_score>target:
+                break
+        else:
+            print("Ball "+str(i)+": "+attributes['batsmen'][current_batsmen_id]['name']+' '+result+' at '+ str(attributes['batsmen'][current_batsmen_id]['runs_scored']))
+            attributes['batsmen'][current_batsmen_id]['out_to']=attributes['bowlers'][current_bowler_id]['name']
+
+            team_wickets+=1
+            attributes['bowlers'][current_bowler_id]['wickets_taken']+=1
+            if team_wickets==10:
+                print("Team is all out at "+str(team_score))
+                break
+            for j in range(len(attributes['batsmen'])):
+                if attributes['batsmen'][j]['balls_faced']==0 and (j not in current_batsmen):
+                    print(str(j)+'  '+attributes['batsmen'][j]['name'])
+            choser=int(input("Choose the next batsman :"))
+            next_batsman_id=choser
+            current_batsmen.remove(current_batsmen_id)
+            current_batsmen.append(next_batsman_id)
+            current_batsmen_id=next_batsman_id
+        time.sleep(3)
+        if i%6==0 and i!=120:
+            os.system('cls')
+            overs=int(i/6)
+            print('Summary :'+str(team_score)+'/'+str(team_wickets)+' after '+str(overs)+' overs Required run rate:'+str(round(((target-team_score)/(20-overs)),2))
+                    +' Required runs:'+str(target-team_score)+'\n\n'+'Current Batsmen :')
+            for j in (current_batsmen):
+                if j!=current_batsmen_id:
+                    print(attributes['batsmen'][j]['name']+"* runs:"
+                    + str(attributes['batsmen'][j]['runs_scored'])+ " balls:"
+                    + str(attributes['batsmen'][j]['balls_faced'])+ " fours:"
+                    + str(attributes['batsmen'][j]['fours'])+ " sixes:"
+                    + str(attributes['batsmen'][j]['sixes']))
+                else:
+                    print(attributes['batsmen'][j]['name']+" runs:"
+                    + str(attributes['batsmen'][j]['runs_scored'])+ " balls:"
+                    + str(attributes['batsmen'][j]['balls_faced'])+ " fours:"
+                    + str(attributes['batsmen'][j]['fours'])+ " sixes:"
+                    + str(attributes['batsmen'][j]['sixes']))
+            print('\n')
+            current_batsmen_id=change_batsman(current_batsmen,current_batsmen_id)
+            for j in range(len(attributes['bowlers'])):
+                if attributes['bowlers'][j]['balls_bowled']<=18 and j!=current_bowler_id:
+                    print(str(j)+' '+attributes['bowlers'][j]['name']+' overs_left:'+str(int((24-attributes['bowlers'][j]['balls_bowled'])/6)))
+            choser=int(input("Choose the id of bowler from above:"))
+            current_bowler_id=choser
+            print('\n')
+    if int(attributes['bowlers'][current_bowler_id]['balls_bowled']%6)==0:
+        print('Summary :'+str(team_score)+'/'+str(team_wickets)+' after '+str(int(i/6))+' overs '+'\n')
+    else:
+        print('Summary :'+str(team_score)+'/'+str(team_wickets)+' after '+str(int(i/6))+str((attributes['bowlers'][current_bowler_id]['balls_bowled'])%6)+' overs '+'\n')
+    for i in range(len(attributes['batsmen'])):
+        if attributes['batsmen'][i]['balls_faced']>0:
+            if str(attributes['batsmen'][i]['out_to'])!='Not Out':
+                print(attributes['batsmen'][i]['name']+' '+str(attributes['batsmen'][i]['runs_scored'])
+                +"("+str(attributes['batsmen'][i]['balls_faced'])+") "+" Out To:"
+                +str(attributes['batsmen'][i]['out_to'])+" 4s:"
+                +str(attributes['batsmen'][i]['fours'])+" 6s:"
+                +str(attributes['batsmen'][i]['sixes']))
+            else:
+                print(attributes['batsmen'][i]['name']+' '+str(attributes['batsmen'][i]['runs_scored'])
+                +"("+str(attributes['batsmen'][i]['balls_faced'])+") "
+                +str(attributes['batsmen'][i]['out_to'])+" 4s:"
+                +str(attributes['batsmen'][i]['fours'])+" 6s:"
+                +str(attributes['batsmen'][i]['sixes']))
+    print('\nBowling Scorecard\n')
+    for i in range(len(attributes['bowlers'])):
+        if attributes['bowlers'][i]['balls_bowled']>0:
+            overs=str(int(attributes['bowlers'][i]['balls_bowled']/6))
+            spare_balls=str(int(attributes['bowlers'][i]['balls_bowled']%6))
+            overs=overs+'.'+spare_balls
+            print(attributes['bowlers'][i]['name']+" "+overs+'-'+str(attributes['bowlers'][i]['dots'])
+            +'-'+str(attributes['bowlers'][i]['runs_conceded'])+'-'+str(attributes['bowlers'][i]['wickets_taken']))
+
+    if team_score>=target:
+        print('\nChasing Team Won by '+str(10-team_wickets)+" wickets")
+    elif target>=team_score:
+        print('\nTeam batting first won by '+str(target-1-team_score)+" runs ")
+    else:
+        print("It's a tie")
